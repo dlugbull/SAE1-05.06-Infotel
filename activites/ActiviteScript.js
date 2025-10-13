1ca8a0d12dc0a35e1de06967b945f7711667733e
Cristiano Martins <cristiano.martins25@orange.fr>
2025-10-13 07:57:55 +0200

    Update ActiviteScript.js

--- activites/ActiviteScript.js
+++ activites/ActiviteScript.js
@@ -3,6 +3,7 @@ function changerLangue(select) {
     if (langue === "fr") {
         window.location.href = "activites.html"; // page française
     } else if (langue === "en") {
-        window.location.href = "actifites_en.html"; // page anglaise
+        window.location.href = "activites_en.html"; // page anglaise
     }
 }
+
